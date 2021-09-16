4bb9b37d406121820633c56f23069d320fc996f7
KUSUM24 <2019kusum.rohra@ves.ac.in>
2021-09-17 01:17:30 +0530

    drag functioning

--- src/Components/VarBox.js
+++ src/Components/VarBox.js
@@ -4,10 +4,16 @@ import { Draggable } from "react-beautiful-dnd";
 export const VarBox = ({ index, value }) => {
   return (
     <>
-      <Draggable key={index} draggableId={index.toString()} index={index}>
+      <Draggable
+        key={index.toString()}
+        draggableId={index.toString()}
+        index={index}
+        id={index.toString()}
+      >
         {(provided) => (
           <div
             key={index}
+            {...provided.draggableProps}
             {...provided.dragHandleProps}
             ref={provided.innerRef}
             className="varBox--button btn"
